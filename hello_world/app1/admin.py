3fc0f86787a60847dd8002d3ae5a47670a2e424b
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-09-14 12:49:39 -0500

    Create, Register, and Manage models with django admin

--- hello_world/app1/admin.py
+++ hello_world/app1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# Import models and the class we want to access in Django Admin
+from .models import Friends
 
 # Register your models here.
+admin.site.register(Friends)
