03c2f823cac6361c5003aee372f6496f194b4d5a
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-09-14 12:56:41 -0500

    Dispalying data from models in a view.

--- hello_world/app1/views.py
+++ hello_world/app1/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, HttpResponse
+# Import friends so we can pass them in the context
+from .models import Friends
 
 # Create your views here.
 def hello_world(request):
+  all_friends = Friends.objects.all()
   context = {
     'title': 'Hello World',
     'name': 'Brodie Giesbrecht',
-    'age': 23
+    'age': 23,
+    'all_friends': all_friends
   }
   return render(request, 'hello_world.html', context)
 
