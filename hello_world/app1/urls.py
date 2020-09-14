19e1478f72e567f6ff11694d3a5890f5c0eefea7
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-09-14 10:29:09 -0500

    Basic URLs and Views

--- hello_world/app1/urls.py
+++ hello_world/app1/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_world)
+    path('hello_world/', views.hello_world),
+    path('a/', views.a),
+    path('b/', views.b)
 ]
--- hello_world/app1/views.py
+++ hello_world/app1/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def hello_world(request):
   return HttpResponse("<h1>Hello and welcome to Django</h1>")
+
+def a(request):
+  return HttpResponse("<h1>AAAAAAAAAAAAAAAAAAA</h1>")
+
+def b(request):
+  return HttpResponse("<h1>Beeeees!</h1>")
